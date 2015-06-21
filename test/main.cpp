8e521332176e2c8a16a91954b79bb7bf6093f30c
Oleg Dolgih <olegvarmy@gmail.com>
2015-06-22 00:04:08 +0600

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Excelsior on 22.06.15.
+//  Copyright (c) 2015 Excelsior. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
