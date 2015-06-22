6966e04f6f66b07402b255f07abd06febbe865bb
Oleg Dolgih <olegvarmy@gmail.com>
2015-06-22 13:14:28 +0600

    add logic for find all words in file and sorting it by most used

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,7 @@
 #include "FileReader.h"
 #include "Finder.h"
 #include <boost/filesystem.hpp>
+#include <map>
 
 bool findFile(const boost::filesystem::path &dirPath, std::string* textToFind) {
     if (!exists(dirPath)) {
@@ -36,12 +37,48 @@ bool findFile(const boost::filesystem::path &dirPath, std::string* textToFind) {
 }
 
 int main() {
-    std::string textForFind;
-    std::cout << "Input substring for find" << std::endl;
-    cin >> textForFind;
-    boost::filesystem::path* path = new boost::filesystem::path("/Users/excelsior/OneDrive/books");
-    if(!findFile(*path, &textForFind)) {
-        std::cout << "Cannot find substring!";
+//    std::string textForFind;
+//    std::cout << "Input substring for find" << std::endl;
+//    cin >> textForFind;
+//    boost::filesystem::path* path = new boost::filesystem::path("/Users/excelsior/OneDrive/books");
+//    if(!findFile(*path, &textForFind)) {
+//        std::cout << "Cannot find substring!";
+//    }
+    
+    std::string filePath;
+    std::cout << "input filename with full path" << endl;
+    std::cin >> filePath;
+    boost::filesystem::path* file = new boost::filesystem::path(filePath);
+    if (!exists(*file)) {
+        std::cout << "file does not exists" << endl;
+        throw std::exception();
     }
+    std::string word;
+    std::ifstream inStream;
+    inStream.open(filePath);
+    std::map<std::string, unsigned int> m;
+    while (inStream >> word) {
+//        std::cout << word << " ";
+        if (m.find(word) == m.end()) {
+            m[word] = 0;
+        } else {
+            unsigned int value = m[word];
+            m[word] = value + 1;
+        }
+    }
+    std::vector<std::pair<unsigned int, std::string>> items;
+    
+    for(std::map<std::string, unsigned int>::const_iterator it = m.begin(); it != m.end(); ++it) {
+        std::pair<unsigned int, std::string> pair;
+        pair = std::make_pair(it->second, it->first);
+        items.push_back(pair);
+//        std::cout << it->first << " => " << it->second << endl;
+    }
+    std::sort(items.rbegin(), items.rend());
+    for(std::vector<std::pair<unsigned int, std::string>>::iterator it = items.begin(); it != items.end(); ++it) {
+        std::pair<unsigned int, std::string> pair = *it;
+        std::cout << pair.first << " = > " << pair.second << endl;
+    }
+
     return 0;
 }
